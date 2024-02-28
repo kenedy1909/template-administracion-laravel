c08458d3bcdb612e8da695712f1f60670a543773
Jhonatan Garcia <jhonatangarcia.r05@gmail.com>
2024-02-28 11:35:14 -0500

    Favicon

--- resources/js/Components/MultiCheckbox.vue
+++ resources/js/Components/MultiCheckbox.vue
@@ -2,7 +2,7 @@
     <!-- Checkbox para seleccionar todos -->
     <div class="mt-3 mb-6 flex items-center">
         <input type="checkbox" id="selectall" v-model="selectAll" @change="selectAllOptions"
-            class="text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500" />
+            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
         <label for="selectall" class="ms-2 font-medium text-gray-700">Seleccionar
             todos</label>
     </div>
